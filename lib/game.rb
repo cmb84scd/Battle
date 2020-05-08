9111cb0923a910212fef064c8a2506c3a2ed5f4d
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-08 11:45:39 +0100

    added switch turns functionality

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,9 @@
 class Game
+  attr_reader :current_turn
+  
   def initialize(player1, player2)
     @players = [player1, player2]
+    @current_turn = player1
   end
 
   def player1
@@ -10,8 +13,18 @@ class Game
   def player2
     @players.last
   end
-  
+
   def attack(player)
     player.receive_damage
+  end
+
+  def switch_turns
+    @current_turn = opponent_of(current_turn)
+  end
+
+  private
+
+  def opponent_of(the_player)
+    @players.select { |player| player != the_player }.first
   end
 end
