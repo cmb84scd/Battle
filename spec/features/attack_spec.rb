e2f5f4a332aceb7f99e4ec1ffd5265553f4c2d94
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-08 10:53:07 +0100

    refactored the controller

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,13 +2,7 @@ feature 'Attack' do
   scenario 'Player 1 attacks player 2' do
     sign_in_and_play
     click_button('Attack')
-    expect(page).to have_content 'Success! Player 2 attacked!'
-  end
-
-  scenario 'reduce Player 2 HP by 10' do
-    sign_in_and_play
-    click_button 'Attack'
-    expect(page).not_to have_content 'Steve: 80HP'
-    expect(page).to have_content 'Steve: 70HP'
+    expect(page).to have_content 'John attacked Steve'
+    click_button('OK')
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,21 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player1, player2) }
   let(:player1) { double :player }
   let(:player2) { double :player }
+
+  describe '#player1' do
+    it 'gets the first player' do
+      expect(game.player1).to eq player1
+    end
+  end
+
+  describe '#player2' do
+    it 'gets the second player' do
+      expect(game.player2).to eq player2
+    end
+  end
 
   describe '#attack' do
     it 'damages a player' do
