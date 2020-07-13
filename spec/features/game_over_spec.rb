3e5cbb58c706960d7d586c532e4aa01417725262
Catriona <catrionamairi@blueyonder.co.uk>
2020-07-13 10:46:41 +0100

    Fix failing test

--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -2,13 +2,13 @@ feature 'Game over' do
   context 'when player 1 reaches 0HP first' do
     before do
       sign_in_and_play
-      attack_and_confirm
-      allow(Kernel).to receive(:rand).and_return(80)
+      14.times { attack_and_confirm }
+      # allow(Kernel).to receive(:rand).and_return(80)
     end
 
     scenario 'Player 1 loses' do
       click_button('Attack')
-      expect(page).to have_content 'John loses!'
+      expect(page).to have_content 'Steve loses!'
     end
   end
 end
